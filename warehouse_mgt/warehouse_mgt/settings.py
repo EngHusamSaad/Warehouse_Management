140144ef047c5b8ea00954be256bb9b115668080
EngHusamSaad <eng.husam.saed@gmail.com>
2024-12-10 08:56:44 +0200

    migrate_models

--- warehouse_mgt/warehouse_mgt/settings.py
+++ warehouse_mgt/warehouse_mgt/settings.py
@@ -74,12 +74,37 @@ WSGI_APPLICATION = 'warehouse_mgt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'mydbproject',
+    #     'USER': 'root',
+    #     'PASSWORD': 'Abcd@1234',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '3306', 
+    
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS':{
+        'host': '192.168.1.87',
+        'port': 3306,  
+        'user': 'root',  
+        'password': 'Abcd@1234',  
+        'database': 'warehouse_mgt',  
     }
-}
+    }
+}    
+        
+
+
 
 
 # Password validation
--- warehouse_mgt/warehousemgt/models.py
+++ warehouse_mgt/warehousemgt/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class User(models.Model):
+        photo=models.TextField()
+        first_name=models.CharField(max_length=50)
+        second_name=models.CharField(max_length=50)
+        identity=models.IntegerField()
+        email=models.EmailField(max_length=254)
+        password=models.TextField()
+        birthday=models.DateField()
+        created_at=models.DateTimeField(auto_now_add=True)
+        updated_at=models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+          return f"{self.first_name} {self.second_name}"  
+    
+class Customer(User):
+    notes=models.TextField()
+    
+
+class Employee(User):
+    postion=models.TextField()
+    
+class Item(models.Model):
+    name_item=models.CharField(max_length=50)
+    icon=models.TextField()
+    sn=models.IntegerField()
+    isAvailble=models.BooleanField()
+    description=models.TextField()
+    sold_date=models.DateField()
+    customers=models.ManyToManyField(Customer,related_name="Items")
+    sold_by=models.IntegerField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
